750a1772c82600ec667b2794cbfeaff1fc1c4ad3
Wes Gibbs <wesgibbs@gmail.com>
2010-10-21 22:25:56 -0400

    Authentication steps for cucumber

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -1,3 +1,22 @@
+Given /^I am signed in$/ do
+  Given %(I am signed out)
+  @me = Factory :user
+  Given %(I am on the sign in page)
+  When  %(I fill in "Email" with "#{@me.email}")
+  And   %(I fill in "Password" with "#{@me.password}")
+  And   %(I press "Sign in")
+  Then  %(I should see "signed in as #{@me.email}")
+end
+
+Given /^I am signed out$/ do
+  Given 'I am on the home page'
+  begin
+    When 'I follow "sign out"'
+  rescue
+  end
+  @me = nil
+end
+
 Given /^an existing user$/ do
-  @user = Factory :user
+  @me = Factory :user
 end
