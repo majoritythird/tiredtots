21fb5f555d3a4c05176acd769c6a3d8ce656370f
Wes Gibbs <wesgibbs@gmail.com>
2010-10-24 09:49:31 -0400

    Iterate over actual date range for sleep graph

--- app/models/child.rb
+++ app/models/child.rb
@@ -1,11 +1,15 @@
 class Child < ActiveRecord::Base
 
   belongs_to :user
-  has_many :sleep_blocks, :dependent => :destroy
+  has_many :sleep_blocks, :order => :start_time, :dependent => :destroy
 
   validates :name, :presence => true, :uniqueness => {:case_sensitive => false, :scope => :user_id}
 
   before_save :set_parameterized_name
+
+  def sleep_data_range
+    (sleep_blocks.first.start_time.to_date..sleep_blocks.last.start_time.to_date)
+  end
 
   def to_param
     parameterized_name
