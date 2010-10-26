0fd403e28f59ea9c3d6d8e2a1351b607b0c9e7cd
Wes Gibbs <wesgibbs@gmail.com>
2010-10-25 22:43:06 -0400

    Install home_run

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,7 +1,7 @@
 # Jan 1 2:15pm
 Time::DATE_FORMATS[:md_hm] = "%b %-1d %-1I:%M%P"
 # Jan 1
-DateTime::DATE_FORMATS[:md] = "%b %-1d"
+DateTime::DATE_FORMATS[:md] = "%b %e"
 # 2:15pm
 Time::DATE_FORMATS[:hmm] = "%-1I:%M%P"
 # 2 pm
