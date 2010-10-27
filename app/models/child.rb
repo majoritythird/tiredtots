91b5ed2dbde695477515662a0f1d10e90458011b
Wes Gibbs <wesgibbs@gmail.com>
2010-10-26 20:33:35 -0400

    Reorder methods

--- app/models/child.rb
+++ app/models/child.rb
@@ -7,6 +7,10 @@ class Child < ActiveRecord::Base
 
   before_save :set_parameterized_name
 
+  def no_data_for_time_block(time)
+    sleep_blocks.finished.none? {|block| block.start_time < time} || sleep_blocks.finished.none? {|block| block.finish_time > time}
+  end
+
   def sleep_block_covering(time)
     sleep_blocks.finished.select do |block|
       block.start_time <= time && block.finish_time >= time
@@ -17,10 +21,6 @@ class Child < ActiveRecord::Base
     last = sleep_blocks.last
     end_date = last.finish_time ? last.finish_time.to_date : last.start_time.to_date
     (sleep_blocks.first.start_time.to_date..end_date)
-  end
-
-  def no_data_for_time_block(time)
-    sleep_blocks.finished.none? {|block| block.start_time < time} || sleep_blocks.finished.none? {|block| block.finish_time > time}
   end
 
   def to_param
