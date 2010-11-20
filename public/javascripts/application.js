5028a991d477c96801dd57e0192ccc6cbb081db2
Wes Gibbs <wesgibbs@gmail.com>
2010-11-19 21:26:40 -0500

    Fix: paging broken by markup change

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,9 +2,10 @@ jQuery(document).ready(function($) {
 
   $('.sleep_blocks a#pager').live('click', function(event) {
     event.preventDefault();
+    var pager = $(this);
     $.get($(this).attr('href'), function(json) {
       $('.sleep_blocks .days').append(json.html);
-      $('.sleep_blocks .actions').replaceWith(json.link);
+      $(pager).parent('div').replaceWith(json.link);
     }, 'json');
   });
 
