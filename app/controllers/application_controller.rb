a9b49f9dfc1619b775c75eff07c565b2bed116a4
Wes Gibbs <wesgibbs@gmail.com>
2011-03-07 14:26:30 -0500

    Checkpoint

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   respond_to :html
 
   before_filter :set_time_zone
+  before_filter :prepare_for_mobile
 
   protected
 
@@ -12,6 +13,15 @@ class ApplicationController < ActionController::Base
       Time.zone = current_user.time_zone
       Chronic.time_class = Time.zone
     end
+  end
+
+  def mobile_device?
+    request.user_agent =~ /Mobile/
+  end
+  helper_method :mobile_device?
+
+  def prepare_for_mobile
+    request.format = :mobile if mobile_device?
   end
 
 end
