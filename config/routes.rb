6999b0a4208c18b99aeac6ac008678ee3a6c8831
Wes Gibbs <wesgibbs@gmail.com>
2011-03-08 10:22:40 -0500

    Mobile views for sleep blocks

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Sleepmonster::Application.routes.draw do
   devise_for :users
-  resources :children, :only => [:index, :new, :create, :edit, :update] do
-    resources :sleep_blocks, :only => [:index, :create, :edit, :update, :destroy] do
+  resources :children, only: [:index, :new, :create, :edit, :update] do
+    resources :sleep_blocks, only: [:index, :new, :create, :edit, :update, :destroy] do
       collection do
-        get "/:date", :to => "sleep_blocks#for_date", :as => "for_date"
-        get "/page/:page", :to => "sleep_blocks#paged", :as => "paged"
+        get "/for/:date", to: "sleep_blocks#for_date", as: "for_date"
+        get "/page/:page", to: "sleep_blocks#paged", as: "paged"
       end
     end
   end
 
-  root :to => "welcome#index"
+  root to: "welcome#index"
 end
