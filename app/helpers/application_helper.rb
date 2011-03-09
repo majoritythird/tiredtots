651e4c3c994170c949868246927f5197cee39ce2
Wes Gibbs <wesgibbs@gmail.com>
2011-03-09 08:19:40 -0500

    Handle unfinished sleep block (mobile)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def data_url
+    @data_url || ""
+  end
+
+  def cache_as(url, unique = false)
+    suffix = unique ? "/#{Time.zone.now.to_f}/" : ""
+    @data_url = url + suffix
+  end
+
 end
